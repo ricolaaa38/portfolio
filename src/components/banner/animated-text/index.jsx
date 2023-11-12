d796fc7d31a59e2ad2a446a093e467d96b311725
nicolas cretton <nicolas.cretton@orange.fr>
2023-11-12 21:14:54 +0100

    fin animation texte title

--- src/components/banner/animated-text/index.jsx
+++ src/components/banner/animated-text/index.jsx
@@ -1,10 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import './style/index.css';
 
 function AnimatedBannerText() {
+  const [titleContent, setTitleContent] = useState('');
+
+  useEffect(() => {
+    const message = 'Nicolas Cretton.';
+    const arr = message.split('');
+    const content = arr.map((char, index) =>
+      char === ' ' ? (
+        <span key={index}>&nbsp;</span>
+      ) : (
+        <span
+          key={index}
+          style={{
+            animationDelay: `${calculateAnimationDelay(arr.length, index)}s`,
+          }}
+        >
+          {char}
+        </span>
+      )
+    );
+
+    setTitleContent(content);
+  }, []);
+
+  const calculateAnimationDelay = (messageLength, index) => {
+    const baseDelay = 0.1;
+    const factor = 0.1;
+    return baseDelay + index * factor + messageLength * factor;
+  };
+
   return (
     <section className="animated-text">
       <div className="bonjour">
-        <h1 className="title">Bonjour!</h1>
+        <h1 className="title" id="titleIntro">
+          {titleContent}
+        </h1>
       </div>
       <div className="bonjour-intro">
         <p>
