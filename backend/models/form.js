991b82fd1b83cc4c0ee890bf54dec81d91e11124
nicolas cretton <nicolas.cretton@orange.fr>
2023-11-20 19:00:02 +0100

    backend et formulaire fonctionnel

--- backend/models/form.js
+++ backend/models/form.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const formSchema = mongoose.Schema({
-    firstName: { type: String, required: true},
-    familyName: { type: String, rquired: true},
+    name: { type: String, required: true},
+    firstname: { type: String, rquired: true},
     email: { type: String, required: true},
     sujet: { type: String, required: true},
     message: { type: String, required: true},
--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,8 +1,81 @@
 import './style/index.css';
 import AnimationBanner from '../banner/animated-background/index';
 import UseIntersectionObserver from '../function/IntersectionObserver';
+import { useState } from 'react';
 
 function Contact() {
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isFormValid, setIsFormValid] = useState(true);
+  const [isSubmitSuccess, setIsSubmitSuccess] = useState(false);
+  const [formData, setFormData] = useState({
+    form: {
+      name: '',
+      firstname: '',
+      email: '',
+      sujet: '',
+      message: '',
+    },
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === 'email') {
+      const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      setIsEmailValid(isValidEmail);
+    }
+
+    setFormData({
+      ...formData,
+      form: {
+        ...formData.form,
+        [name]: value,
+      },
+    });
+  };
+
+  const isFormDataValid = () => {
+    return (
+      formData.form.name.trim() !== '' &&
+      formData.form.firstname.trim() !== '' &&
+      formData.form.email.trim() !== '' &&
+      formData.form.sujet.trim() !== '' &&
+      formData.form.message.trim() !== '' &&
+      isEmailValid
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isFormDataValid()) {
+      console.error('Veuillez remplir tous les champs correctement.');
+      setIsFormValid(false);
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:4000/api/formulaire', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log('Données envoyées avec succès');
+        setIsFormValid(true);
+        setIsSubmitSuccess(true);
+      } else {
+        console.error("Erreur lors de l'envoi des données");
+        setIsFormValid(false);
+      }
+    } catch (error) {
+      console.error('Erreur lors de la requête :', error);
+      setIsFormValid(false);
+    }
+  };
   UseIntersectionObserver('.hidden');
   return (
     <section className="portfolio-contact" id="contact">
@@ -16,11 +89,22 @@ function Contact() {
             ou via le formulaire ci-contre
           </p>
         </div>
-        <form action="#" method="post">
+        {isFormValid || (
+          <p className="error-submit-message">
+            Veuillez remplir tous les champs correctement.
+          </p>
+        )}
+        <form onSubmit={handleSubmit}>
           <div className="label-container">
             <div className="label-input-container">
               <label htmlFor="name">Votre nom</label>
-              <input type="text" name="name" id="name" placeholder="Dupont" />
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Dupont"
+                onChange={handleChange}
+              />
             </div>
             <div className="label-input-container">
               <label htmlFor="firstname">Votre prénom</label>
@@ -29,6 +113,7 @@ function Contact() {
                 name="firstname"
                 id="firstname"
                 placeholder="Martin"
+                onChange={handleChange}
               />
             </div>
           </div>
@@ -38,9 +123,16 @@ function Contact() {
             name="email"
             id="email"
             placeholder="monadresse@email.com"
+            onChange={handleChange}
+            className={!isEmailValid ? 'invalid' : ''}
           />
-          <label for="sujet">Sujet</label>
-          <select name="sujet" id="sujet">
+          {!isEmailValid && (
+            <p className="error-message">
+              Veuillez entrer une adresse email valide.
+            </p>
+          )}
+          <label htmlFor="sujet">Sujet</label>
+          <select name="sujet" id="sujet" onChange={handleChange}>
             <option value=""></option>
             <option value="mesProjets">
               Une question sur l'un de mes projets?
@@ -55,9 +147,16 @@ function Contact() {
             placeholder="Bonjour, je vous contacte car ..."
             cols="30"
             rows="10"
+            onChange={handleChange}
           ></textarea>
           <input type="submit" value="Envoyer" />
         </form>
+        {isSubmitSuccess && (
+          <div className="confirmation-popup">
+            <p>Merci ! Votre formulaire a été envoyé avec succès.</p>
+            <button onClick={() => setIsSubmitSuccess(false)}>Fermer</button>
+          </div>
+        )}
       </div>
     </section>
   );
