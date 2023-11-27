5a5849001d22cd91cbc3053bec4f49829862c649
nicolas cretton <nicolas.cretton@orange.fr>
2023-11-27 22:57:49 +0100

    maj

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MON_ID_MONGODB,
     app.use(express.json());
 
     app.use((req, res, next) => {
-        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', 'https://nicolas-cretton.com');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         next();
